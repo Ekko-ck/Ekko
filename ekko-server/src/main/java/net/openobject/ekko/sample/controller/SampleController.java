430a2bb1460045d5cb95f841d1715ee4eadc01f2
sehoone <sehoon1989@gmail.com>
2021-03-08 21:59:10 +0900

    first commit

---
+++ ekko-server/src/main/java/net/openobject/ekko/sample/controller/SampleController.java
@@ -0,0 +1,20 @@
+package net.openobject.ekko.sample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/sample")
+public class SampleController {
+	
+	//@RequestMapping(path="hell")
+	@GetMapping("/hello/{input}")
+	public String sayHello(@PathVariable String input) {
+		
+		System.out.print("hello");
+		
+		return input;
+	}
+}
