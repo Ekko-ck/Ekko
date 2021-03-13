de8e67ed147fffa2848f409549c35e1416c0865f
kis6905 <kis6905@gmail.com>
2021-03-13 15:39:10 +0900

    spring cloud config 설정 추가

--- ekko-server/src/main/java/net/openobject/ekko/sample/controller/SampleController.java
+++ ekko-server/src/main/java/net/openobject/ekko/sample/controller/SampleController.java
@@ -1,5 +1,6 @@
 package net.openobject.ekko.sample.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/sample")
 public class SampleController {
 	
-	//@RequestMapping(path="hell")
 	@GetMapping("/hello/{input}")
 	public String sayHello(@PathVariable String input) {
-		
 		System.out.print("hello");
-		
 		return input;
 	}
+	
+    @Value("${database.username}")
+    private String dbUsername;
+    
+    @Value("${database.password}")
+    private String dbPassword;
+
+    @GetMapping("/cloud/config")
+    public String getCloudConfig() {
+        return String.format("username: %s, password: %s", dbUsername, dbPassword);
+    }
+    
 }
