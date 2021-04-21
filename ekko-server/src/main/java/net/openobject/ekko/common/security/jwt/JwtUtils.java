d369178d1340fc8590b71dc0d9d0a9c764f9ec09
sehoone <sehoon1989@gmail.com>
2021-04-21 23:05:12 +0900

    JWT 만료시간 제거

--- ekko-server/src/main/java/net/openobject/ekko/common/security/jwt/JwtUtils.java
+++ ekko-server/src/main/java/net/openobject/ekko/common/security/jwt/JwtUtils.java
@@ -44,14 +44,14 @@ public class JwtUtils {
 		UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
 
 		return Jwts.builder().setSubject((userPrincipal.getUsername())).setIssuedAt(new Date())
-				.setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+				//.setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
 				.signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
 	}
 	
 	public String generateJwtToken(String token) {
 		String username = getUserNameFromJwtToken(token);
 		return Jwts.builder().setSubject((username)).setIssuedAt(new Date())
-				.setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+				//.setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
 				.signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
 	}
 
