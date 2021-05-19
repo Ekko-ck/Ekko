db9d721edac0d492a8d02e8a705710dc182a67fe
sehoone <sehoon1989@gmail.com>
2021-05-20 01:19:04 +0900

    swagger 적용

--- ekko-server/src/main/java/net/openobject/ekko/sample/dto/SampleUserRequest.java
+++ ekko-server/src/main/java/net/openobject/ekko/sample/dto/SampleUserRequest.java
@@ -1,16 +1,24 @@
 package net.openobject.ekko.sample.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@ApiModel(value = "샘플사용자정보", description = "샘플사용자정보")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SampleUserRequest {
 	
+	@ApiModelProperty(value = "사용자시퀀스", required = true)
 	private Long id;
+	
+	@ApiModelProperty(value = "사용자ID", required = true)
 	private String userId;
+	
+	@ApiModelProperty(value = "사용자명")
 	private String userName;
 	
 }
