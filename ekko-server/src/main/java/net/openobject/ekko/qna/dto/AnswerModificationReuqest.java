62f3bdf9b798fd3a412e5b56a71ddd2007d53621
kis6905 <kis6905@gmail.com>
2021-06-05 13:25:05 +0900

    API 문서 추가, sort 버그 수정

--- ekko-server/src/main/java/net/openobject/ekko/qna/dto/AnswerModificationReuqest.java
+++ ekko-server/src/main/java/net/openobject/ekko/qna/dto/AnswerModificationReuqest.java
@@ -1,10 +1,14 @@
 package net.openobject.ekko.qna.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+@ApiModel(value = "답변 수정 Form", description = "답변 수정 Form")
 @Data
 public class AnswerModificationReuqest {
 	
+	@ApiModelProperty(value = "내용", required = true)
 	private String contents;
 	
 }
