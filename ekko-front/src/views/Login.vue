402e6552bcc58dc0d3d76a740994644bbaac1cb6
SeHoon <sehoon1989@gmail.com>
2021-06-10 00:26:03 +0900

    회원가입, 로그인 화면 레이아웃 수정

--- ekko-front/src/views/Login.vue
+++ ekko-front/src/views/Login.vue
@@ -1,52 +1,44 @@
 <template>
-  <div>
-  <v-app id="inspire">
-    <v-container>
-      <v-layout wrap>
-        <v-flex sm12 md6 offset-md3>
-          <v-card elevation="4" light tag="section">
-            <v-card-title>
-              <v-layout align-center justify-space-between>
-                <v-flex>
-                  <v-img class="ml-3" contain height="48px" position="center right" src="https://www.mobygames.com/images/i/12/25/1435075.png"></v-img>
-                </v-flex>
-              </v-layout>
-            </v-card-title>
-            <v-divider></v-divider>
-            <v-card-text>
-              <p>Sign in with your username and password:</p>
-              <v-form>
-                <v-text-field
-                              outline
+   <v-app id="inspire">
+      <v-main>
+         <v-container fluid fill-height>
+            <v-layout align-center justify-center>
+               <v-flex xs12 sm8 md4>
+                  <v-card class="elevation-12">
+                     <v-toolbar dark color="primary">
+                        <v-toolbar-title>Ekko Login</v-toolbar-title>
+                     </v-toolbar>
+                     <v-card-text>
+                        <v-form>
+                           <v-text-field
+                              prepend-icon="mdi-account"
+                              name="login"
                               label="Username"
                               type="text"
-                              v-model="userId"></v-text-field>
-                <v-text-field
-                              outline
-                              hide-details
+                              v-model="userId"
+                           ></v-text-field>
+                           <v-text-field
+                              id="password"
+                              prepend-icon="mdi-lock"
+                              name="password"
                               label="Password"
                               type="password"
-                              v-model="password"></v-text-field>
-              </v-form>
-            </v-card-text>
-            <v-divider></v-divider>
-            <v-card-actions>
-              <v-btn @click="handleJoin" color="info">
-                <v-icon left>mdi-account</v-icon>
-                Join
-              </v-btn>
-              <v-spacer></v-spacer>
-              <v-btn @click="handleSigninUser" color="info">
-                <v-icon left>mdi-lock</v-icon>
-                Login
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
-    </v-app>
-  </div>
+                              v-on:keyup.enter="handleSigninUser"
+                              v-model="password"
+                           ></v-text-field>
+                        </v-form>
+                     </v-card-text>
+                     <v-card-actions>
+                        <v-btn color="primary" @click="handleSignupUser">Join</v-btn>
+                        <v-spacer></v-spacer>
+                        <v-btn color="primary" @click="handleSigninUser">Login</v-btn>
+                     </v-card-actions>
+                  </v-card>
+               </v-flex>
+            </v-layout>
+         </v-container>
+      </v-main>
+   </v-app>
 </template>
 
 <script>
@@ -67,12 +59,13 @@ export default {
     // 로그인 처리
     async handleSigninUser () {
       const resSigninUser = await this.signinUser({ userId: this.userId, password: this.password })
+      console.log('resSigninUser ' + resSigninUser)
       if (resSigninUser != null) {
         this.$router.push({ name: 'Question' })
       }
     },
     // 회원가입 화면이동
-    handleJoin () {
+    handleSignupUser () {
       this.$router.push({ name: 'Join' })
       // this.$router.push('/join')
     }
