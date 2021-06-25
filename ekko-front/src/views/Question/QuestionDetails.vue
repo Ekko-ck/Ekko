03af56d1043ababd0fa73f42ab79b9a395f3923e
iskwon <kis6905@gmail.com>
2021-06-25 17:42:36 +0900

    질문 상세 작업 중

--- ekko-front/src/views/Question/QuestionDetails.vue
+++ ekko-front/src/views/Question/QuestionDetails.vue
@@ -1,24 +1,85 @@
 <template>
   <div>
-    <div class="text-h5">
-      {{ question.title }}
+    <div>
+      <v-row class="align-items-center">
+        <v-col cols="2" class="text-center">
+          <v-btn
+            icon
+            color="blue-grey darken-1"
+            @click="handleClickVoteUp"
+          >
+            <v-icon>mdi-chevron-up-circle</v-icon>
+          </v-btn>
+          <br/>
+          {{ question.votes }}
+          <br/>
+          <v-btn
+            icon
+            color="blue-grey darken-1"
+            @click="handleClickVoteDown"
+          >
+            <v-icon>mdi-chevron-down-circle</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col cols="10">
+          <div class="text-h5">
+            {{ question.title }}
+          </div>
+          <div class="pt-3 pb-3">
+            <v-chip
+              v-for="(tag, index) in question.tags"
+              :key="tag+index"
+              small
+              color="#E3F2FD"
+              class="mr-2"
+            >
+              {{ tag }}
+            </v-chip>
+          </div>
+        </v-col>
+      </v-row>
     </div>
-    <div class="pt-3 pb-3">
-      <v-chip
-        v-for="(tag, index) in question.tags"
-        :key="tag+index"
-        small
-        color="#E3F2FD"
-        class="mr-2"
-      >
-        {{ tag }}
-      </v-chip>
+
+    <v-divider class="mt-2 mb-2"></v-divider>
+
+    <div class="contents">
+      {{ question.contents }}
     </div>
-    <hr class="division-line" />
+
+    <v-divider class="mt-2 mb-2"></v-divider>
+
+    <div>
+      <v-row class="align-items-center">
+        <v-col cols="1">
+          <v-avatar
+            color="teal"
+            dark
+            size="30"
+          >
+            <span class="white--text">{{ userNameForAvatar }}</span>
+          </v-avatar>
+        </v-col>
+        <v-col cols="3" class="pl-4">
+          {{ this.user.userNm }}
+        </v-col>
+        <v-col cols="8" class="text-right">
+          <span class="text--secondary">{{ question.registeredAt }}</span>
+        </v-col>
+      </v-row>
+    </div>
+
+    <v-alert
+      dark
+      color="blue-grey darken-1"
+      class="pt-2 pb-2 mt-5 mb-2"
+    >
+      <span class="text-h6">{{ answerCount }} Answer</span>
+    </v-alert>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'QuestionDetails',
@@ -35,14 +96,33 @@ export default {
     console.log(this.question)
   },
   computed: {
+    ...mapGetters('auth', ['user']),
+    userNameForAvatar () {
+      return this.user.userNm.substring(0, 2)
+    },
+    answerCount () {
+      return this.question.answers ? this.question.answers.length : '0'
+    }
   },
   methods: {
+    handleClickVoteUp () {
+      console.log('up')
+    },
+    handleClickVoteDown () {
+      console.log('down')
+    }
   }
 }
 </script>
 
 <style scoped>
-.division-line {
-  border-top: 1px solid #cecece;
+.contents {
+  min-height: 150px;
+}
+.text-center {
+  text-align: center;
+}
+.align-items-center {
+  align-items: center;
 }
 </style>
