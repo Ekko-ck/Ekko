ee87b6087067110e12092418ee548a176447a411
SeHoon <sehoon1989@gmail.com>
2021-07-06 23:56:02 +0900

    회원가입, 로그인 화면 센터정렬

--- ekko-front/src/views/Join.vue
+++ ekko-front/src/views/Join.vue
@@ -1,56 +1,50 @@
 <template>
-   <v-app id="inspire">
-      <v-main>
-         <v-container fluid fill-height>
-            <v-layout align-center justify-center>
-               <v-flex xs12 sm8 md4>
-                  <v-card class="elevation-12">
-                     <v-toolbar dark color="primary">
-                        <v-toolbar-title>Ekko Join</v-toolbar-title>
-                     </v-toolbar>
-                     <v-card-text>
-                        <v-form>
-                           <v-text-field
-                              prepend-icon="mdi-account"
-                              name="login"
-                              label="Username"
-                              type="text"
-                              v-model="userId"
-                           ></v-text-field>
-                           <v-text-field
-                              id="password"
-                              prepend-icon="mdi-lock"
-                              name="password"
-                              label="Password"
-                              type="password"
-                              v-model="password"
-                           ></v-text-field>
-                           <v-text-field
-                              prepend-icon="mdi-email"
-                              name="userEmailAddr"
-                              label="Email"
-                              type="text"
-                              v-model="userEmailAddr"
-                           ></v-text-field>
-                        </v-form>
-                     </v-card-text>
-                     <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn color="primary" @click="handleSignupUser">SingUp</v-btn>
-                     </v-card-actions>
-                  </v-card>
-               </v-flex>
-            </v-layout>
-         </v-container>
-      </v-main>
-   </v-app>
+   <div class="login-form">
+      <v-layout align-center justify-center>
+         <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+               <v-toolbar dark color="primary">
+                  <v-toolbar-title>Ekko Join</v-toolbar-title>
+               </v-toolbar>
+               <v-card-text>
+                  <v-form>
+                     <v-text-field
+                        prepend-icon="mdi-account"
+                        name="login"
+                        label="Username"
+                        type="text"
+                        v-model="userId"
+                     ></v-text-field>
+                     <v-text-field
+                        id="password"
+                        prepend-icon="mdi-lock"
+                        name="password"
+                        label="Password"
+                        type="password"
+                        v-model="password"
+                     ></v-text-field>
+                     <v-text-field
+                        prepend-icon="mdi-email"
+                        name="userEmailAddr"
+                        label="Email"
+                        type="text"
+                        v-model="userEmailAddr"
+                     ></v-text-field>
+                  </v-form>
+               </v-card-text>
+               <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="primary" @click="handleSignupUser">SingUp</v-btn>
+               </v-card-actions>
+            </v-card>
+         </v-flex>
+      </v-layout>
+   </div>
 </template>
 <script>
 import { mapActions } from 'vuex'
 export default {
   name: 'Join',
-  components: {
-  },
   data () {
     return {
       userId: '',
@@ -72,3 +66,11 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+.login-form {
+  height: calc(100vh - 90px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
